4961d1e4c92b49ba24eefb89e5c4bbc0a937915e
Vasjan Çupri <vasjancupri04@gmail.com>
2023-06-04 17:08:58 +0200

    Added the wordfrequencycounter functions

--- src/components/wordCounter/WordCounter.jsx
+++ src/components/wordCounter/WordCounter.jsx
@@ -1,12 +1,54 @@
 import "./WordCounter.scss"
+import { useState } from "react"
+
 function WordCounter() {
+  const prepositionArray = [
+    "the",
+    "etc",
+    "of",
+    "but",
+    "so",
+    "in",
+    "to",
+    "per",
+    "as",
+    "till",
+    "on",
+    "a"
+  ]
+  const [text, setText] = useState("")
+
+  const wordFormatter = (word) => {
+    const formattedWord = word.toLowerCase().replace(/[^a-z0-9]+/gi, "");
+    return formattedWord;
+  };
+
+  const handleSubmit = () => {
+    if (!text) return "no words"
+    const separatedTextArray = text.split(" ")
+    const frequency = {}
+    separatedTextArray.map((word) => {
+      const formattedWord = wordFormatter(word)
+      if (frequency[formattedWord]) frequency[formattedWord] += 1
+      else {
+        if (prepositionArray.includes(formattedWord)) return
+        frequency[formattedWord] = frequency[formattedWord] = 1
+      }
+    })
+    console.log(frequency)
+  }
+
   return (
     <div className="wc-container">
       <h2>WordCounter</h2>
-      <div >
-
-      <textarea name="" id="" cols="30" rows="10"></textarea>
-      <input type="button" value={"submit"} />
+      <div>
+        <textarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          cols="30"
+          rows="10"
+        />
+        <input type="button" value="submit" onClick={handleSubmit} />
       </div>
     </div>
   )
